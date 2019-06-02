f94fe68013c650fc90a01300d76fc3dfaed58a4a
ionut.iacob <ionut.iacob17@gmail.com>
2019-06-02 10:11:34 +0300

    Added dist versions

---
+++ dist-react-grid/precache-manifest.34400b3aff6f700ce05288a8db757b0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e6a92c2d19070e6ecee0644af41628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96457f51c299665936c9",
+    "url": "/static/css/main.1b74eabe.chunk.css"
+  },
+  {
+    "revision": "258cb1cab7b04a5c8864",
+    "url": "/static/js/2.aee76bf5.chunk.js"
+  },
+  {
+    "revision": "96457f51c299665936c9",
+    "url": "/static/js/main.aa326eeb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
